f59a6adabe4f289eff98d2e584bf4bca286e53e6
MarcosPaes01 <marcospaes35@gmail.com>
2025-06-03 01:18:03 -0300

    Inserting reports html in tests

--- k6-load-test/scripts/test-api.js
+++ k6-load-test/scripts/test-api.js
@@ -1,5 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 
 export const options = {
     vus: 500,               // Número fixo de usuários virtuais
@@ -19,4 +21,10 @@ export default function () {
   });
 
   sleep(1); // simula tempo de navegação entre requisições
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
 }
